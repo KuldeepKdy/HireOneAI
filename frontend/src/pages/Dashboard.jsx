4a4934fd843821b05169d581800b388980b4069a
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2026-09-18 16:11:13 +0530

    dashboard page sidebar frontend setup and integration

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../utils/axios";
+import {motion} from "motion/react";
 
-function Dashboard({ user,setUser }) {
+function Dashboard({ user, setUser }) {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await api.get("/api/auth/logout");
+      if (response.data.success) {
+        setUser(null);
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div>{user?.name}</div>
-  )
+    <div className="bg-white min-h-screen text-[#0A0A0A] font-sans flex">
+      <Sidebar
+        user={user}
+        onNewInterview={() => navigate("/interview")}
+        onLogout={handleLogout}
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        mobileOpen={mobileOpen}
+        setMobileOpen={setMobileOpen}
+      />
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
